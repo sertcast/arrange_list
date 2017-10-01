a8659cebbe9be535ce111e387b57ab7511ed1de5
Mert Kaya <30465847+sertcast@users.noreply.github.com>
2017-10-01 15:17:10 -0400

    Initial Commit

---
+++ arrange_list/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  arrange_list
+//
+//  Created by Mert Kaya on 2017-10-01.
+//  Copyright © 2017 Mert Kaya. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
